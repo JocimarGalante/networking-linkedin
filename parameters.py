0f31eedb16f75452aa89c10694504db4ac20b711
JocimarGalante <jocimarcgalante@hotmail.com>
2024-04-13 09:20:03 -0300

    Commit

---
+++ parameters.py
@@ -0,0 +1,6 @@
+file_name = 'connections.csv'
+linkedin_username = 'email@gmail.com'
+linkedin_password = 'PASSWORD'
+keywords = 'DevOps,Linux,Python,AWS'
+ignore_list = 'Person1,Person2'
+till_page = 10
